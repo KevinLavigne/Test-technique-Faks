0ee5e9da9111f190d7bde098f9fe6b1b1f4969c7
KevinLavigne <lavigne.kevin02@gmail.com>
2023-01-21 21:29:05 +0100

    Rename pages file name and function inside it

---
+++ src/pages/NewClaim.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NewClaim() {
+	return (
+		<div>
+			<h1 className="text-5xl font-semibold text-center">welcome on Page 2</h1>
+		</div>
+	);
+}
+
+export default NewClaim;
---
+++ src/pages/User.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function User() {
+	return (
+		<div>
+			<h1 className="text-5xl font-semibold text-center">welcome on Page 3</h1>
+		</div>
+	);
+}
+
+export default User;
